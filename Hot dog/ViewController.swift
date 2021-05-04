1814c99aabade56f46e87d829f0f32949af4ab90
yj.user <apffhs41@naver.com>
2021-05-05 00:41:41 +0900

    Initial Commit

---
+++ Hot dog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hot dog
+//
+//  Created by youngjun kim on 2021/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
